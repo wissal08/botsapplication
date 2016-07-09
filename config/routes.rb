8d56f664fa5112e55ecdab972e2784cc17a496e0
wissal08 <wissal.jrad@gmail.com>
2016-07-09 21:08:58 +0000

    Added pages controller, home viiews and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'Pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
